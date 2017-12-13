6d60b3735118aa1ec4fd0f68c40d84681e818957
Eric <ericso0303@gmail.com>
2017-12-12 18:58:01 -0800

    adds inquirer

--- Programmers.js
+++ Programmers.js
@@ -1,3 +1,4 @@
+var inquirer = require('inquirer')
 class Programmer  {
   constructor(name, title, age, language){
     this.name = name
@@ -9,11 +10,32 @@ class Programmer  {
   info(){
     console.log(this.name, this.title, this.age, this.language)
   }
-
   
 }
 
 var Eric = new Programmer('Eric', 'noob', 80, 'mySQL')
 
-Eric.info()
+inquirer.prompt([
+  {
+    name: 'name',
+    message: "What is your name?"
+  },
+  {
+    name: 'title',
+    message: "What is your title?"
+  },
+  {
+    name: 'age',
+    message: "What is your age?"
+  },
+  {
+    name: 'language',
+    message: "What is your language?"
+  },
+]).then(function( answers) {
+  const {name, title, age, language} = answers
+  var newPerson = new Programmer(name, title, age, language)
+  newPerson.info()
+})
+// Eric.info()
 
