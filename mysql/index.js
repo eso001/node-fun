bde2b58fe5bef3bfad13b74cbeb5a27ae67d18a0
Eric <ericso0303@gmail.com>
2017-12-19 20:45:12 -0800

    adds queries

--- mysql/index.js
+++ mysql/index.js
@@ -2,7 +2,25 @@ var mysql = require('mysql')
 
 var connection = mysql.createConnection({
   host: 'localhost',
+  port: '8889',
   user: 'root',
   password: 'root',
-  databbase: 
+  database: 'programming_db'
 })
+
+connection.connect(function(err) {
+  if (err) throw err;
+  //threadId 
+  console.log('connected as id: ' + connection.threadId)
+  afterConnection()
+})
+
+function afterConnection() {
+  connection.query('SELECT * FROM programming_languages', function(err, res) {
+    if(err) throw err;
+
+    console.log(res)
+    connection.end()
+  })
+}
+
